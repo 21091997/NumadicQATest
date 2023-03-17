c8e46200c66b0e514f5af48b7ed060566ca745f6
User <User@DESKTOP-GU0DP2S>
2023-03-17 20:38:36 +0530

    Push

--- src/PackOne/Class1.java
+++ src/PackOne/Class1.java
@@ -1,5 +1,10 @@
 package PackOne;
 
-public class Class1 {
-
+public class Class1 
+{
+public static void main(String[] args) 
+{
+	System.out.println("Utkarsha");
+	System.out.println("Kulkarni");
+}
 }
